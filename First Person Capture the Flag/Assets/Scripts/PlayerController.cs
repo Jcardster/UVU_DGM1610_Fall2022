74e2d5870f1bc9fefc58a5e1abaf9547f2cad197
Jcardster42 <10875523@my.uvu.edu>
2022-12-08 00:21:17 -0700

    Working simple beta for first person
    Player moves around and can collect pickups

--- First Person Capture the Flag/Assets/Scripts/PlayerController.cs
+++ First Person Capture the Flag/Assets/Scripts/PlayerController.cs
@@ -14,18 +14,41 @@ public class PlayerController : MonoBehaviour
     private float rotX;
     private Camera camera;
     private Rigidbody rb;
+    //private Weapon weapon;
 
     void Awake()
     {
-        //Get Components
-        camera = Camera.main;
-        rb = GetComponent<Rigidbody>();
+        //weapon = GetComponent<Weapon>();
     }
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        //Get Components
+        camera = Camera.main;
+        rb = GetComponent<Rigidbody>();
+
+        //Initialize the UI
+        //GameUI.instance.UpdateHealthBar(curHp, maxHp);
+        //GameUI.instance.UpdateScoreText(0);
+        //GameUI.instance.UpdateAmmoText(weapon.curAmmo, weapon.maxAmmo);
+    }
+
+    public void TakeDamage(int damage)
+    {
+        //curHp -= damage;
+
+        //if(curHp <= 0)
+        //{
+            //Die();
+        //}
+        //GameUI.instance.UpdateHealthBar(curHp, maxHp);
+    }
+
+    void Die()
+    {
+        //GameManager.instance.LoseGame();
+        Debug.Log("Player had died! Game Over");
     }
 
     // Update is called once per frame
@@ -34,19 +57,36 @@ public class PlayerController : MonoBehaviour
         Move();
         CameraLook();
 
+        //Fire Button
+        if(Input.GetButton("Fire1"))
+        {
+            //if(weapon.CanShoot())
+            //{
+            //    weapon.Shoot();
+            //}
+            Debug.Log("Player shoots!");
+        }    
+
+        //Jump Button
         if(Input.GetButtonDown("Jump"))
         {
             Jump();
         }
+
+        //Don't do anything if the game is paused
+        //if (GameManager.instance.gamePaused == true)
+            //return;
     }
 
 
     void Move()
     {
-        float x = Input.GetAxis("Horizontal") * moveSpeed;
-        float z = Input.GetAxis("Vertical") * moveSpeed;
-
-        rb.velocity = new Vector3(x, rb.velocity.y, z);
+        float x = Input.GetAxis("Horizontal") * moveSpeed; //Getting input for left and right movement
+        float z = Input.GetAxis("Vertical") * moveSpeed; //Getting input for forward and back movement
+        //Walk in the direction we are facing
+        Vector3 dir = transform.right * x + transform.forward * z;
+        dir.y = rb.velocity.y;
+        rb.velocity = dir; //Applying velocity to the x and z axis to drive player
     }
 
 
@@ -54,6 +94,11 @@ public class PlayerController : MonoBehaviour
     {
         float y = Input.GetAxis("Mouse X") * lookSensitivity; //Look up and down
         rotX += Input.GetAxis("Mouse Y") * lookSensitivity; //Look left and right
+
+        rotX = Mathf.Clamp(rotX, minLookX, maxLookX); //Constrains Camera
+        // Drives Camera rotation
+        camera.transform.localRotation = Quaternion.Euler(-rotX, 0, 0);
+        transform.eulerAngles += Vector3.up * y;
     }
 
     void Jump()
@@ -64,6 +109,20 @@ public class PlayerController : MonoBehaviour
         {
             rb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
         }
+    }
+
+    public void GiveHealth(int amountToGive)
+    {
+        //curHp = Mathf.Clamp(curHp + amountToGive, 0, maxHp);
+        //GameUI.instance.UpdateHealthBar(curHp, maxHp);
+        Debug.Log("Player has been healed!");
+    }
+
+    public void GiveAmmo(int amountToGive)
+    {
+        //weapon.curAmmo = Mathf.Clamp(weapon.curAmmo + amountToGive, 0, weapon.maxAmmo);
+        //GameUI.instance.UpdateAmmoText(weapon.curAmmo, weapon.maxAmmo);
+        Debug.Log("Player has collected ammo!");
     }
 
 }
